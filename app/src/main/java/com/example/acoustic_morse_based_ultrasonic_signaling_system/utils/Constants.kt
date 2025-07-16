05a38715f19dd0a113e07fa8f79cfea37de33afb
Abhi95081 <abhirou7@gmail.com>
2025-07-16 17:51:50 +0530

    constant commit

---
+++ app/src/main/java/com/example/acoustic_morse_based_ultrasonic_signaling_system/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.acoustic_morse_based_ultrasonic_signaling_system.utils
+
+
+object Constants {
+    const val SAMPLE_RATE = 44100
+    const val FREQ_DOT = 19000.0
+    const val FREQ_DASH = 20000.0
+}
